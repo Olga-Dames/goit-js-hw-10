68963d87db53dc8942174ccc79edceb2723570fd
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-01-29 21:38:07 +0200

    final v2

--- src/index.js
+++ src/index.js
@@ -23,49 +23,54 @@ function onInputSearch(e) {
     return;
   }
 
-  fetchCoutries(inputValue)
-  .then(createCoutriesList)
-  .catch(onErrorNotify);   
+  fetchCoutries(inputValue).then(createCoutriesList).catch(onErrorNotify);
 }
 
-
-function createCoutriesList (country) {
-const countryLength = country.length; 
-if(countryLength > 10) {
-  return Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
-} 
-if (countryLength > 1 && countryLength <= 10) {
-  return arrayOfCountries(country)
-}
-if (countryLength === 1) {
-  return countryCard(country)
-}
+function createCoutriesList(country) {
+  const countryLength = country.length;
+  if (countryLength > 10) {
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name'
+    );
+  }
+  if (countryLength > 1 && countryLength <= 10) {
+    return arrayOfCountries(country);
+  }
+  if (countryLength === 1) {
+    return countryCard(country);
+  }
 }
 
 function onErrorNotify(error) {
   return Notiflix.Notify.failure('Oops, there is no country with that name');
 }
 
-function arrayOfCountries(country){
-  const countriesArray = country.map(
-    ({flags, name}) =>
-    `<li class="country__descr">
+function arrayOfCountries(country) {
+  const countriesArray = country
+    .map(
+      ({ flags, name }) =>
+        `<li class="country__descr">
     <img class="country__icon" src="${flags.svg}" alt="${name.official}" width="60">
-    <p class="country__name">${name.official}</p></li>`)
-  .join('')
-  refs.countryInfo.innerHTML = ''
+    <p class="country__name">${name.official}</p></li>`
+    )
+    .join('');
+  refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = countriesArray;
 }
 
-function countryCard(country){
-  const card = country.map(
-    ({flags,name,capital,population,languages}) =>
-    `<div class="flex"><img class="country__icon" src="${flags.svg}" alt="${name.official}" width="100"></img>
+function countryCard(country) {
+  const card = country
+    .map(
+      ({ flags, name, capital, population, languages }) =>
+        `<div class="flex"><img class="country__icon" src="${flags.svg}" alt="${
+          name.official
+        }" width="100"></img>
      <h2 class="country__descr">${name.official}</h2></div>
      <p class="country__name"><b>Capital: </b>${capital}</p>
      <p class="country__name"><b>Population: </b>${population}</p>
      <p class="country__name"><b>Languages: </b>${Object.values(languages)}</p>`
-     ).join('')
+    )
+    .join('');
   refs.countryList.innerHTML = '';
   refs.countryInfo.innerHTML = card;
 }
